7c2bee497c5f1f106ba9d152658ae90365275200
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-04-29 14:22:35 +0900

    [level 0] Title: 글자 지우기, Time: 0.56 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String[] my_string_arr = my_string.split("");
+        for(int i=0; i<indices.length; i++){
+            my_string_arr[indices[i]] = "";
+        }
+        return String.join("",my_string_arr);
+    }
+}
