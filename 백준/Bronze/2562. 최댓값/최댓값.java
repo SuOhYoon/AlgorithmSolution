378865639b4bcffef7dd6d21568ea4451addeadf
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-05-19 23:40:44 +0900

    [Bronze III] Title: 최댓값, Time: 168 ms, Memory: 17508 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
-public class Main {
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int[] number = {s.nextInt(), s.nextInt(), s.nextInt(), 
-						s.nextInt(), s.nextInt(), s.nextInt(), 
-						s.nextInt(), s.nextInt(), s.nextInt()};
-		
-		int count =0;
-		int max = 0;
-		int index = 0;
-		
-		for(int value:number) {
-			count++;
-		
-			if(value > max) { 
-			max = value;
-			index = count;
-		}
-	}
-		System.out.print(max + "\n" + index);
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[9];
+        int max = 0;
+        int No = 0;
+        
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(max < arr[i]){
+                max = arr[i];
+                No = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(No+1);
+    }
 }
