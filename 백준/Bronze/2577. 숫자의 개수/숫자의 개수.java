d2dd67fbc7c9a67bf8f1edb6bcc41897a936d156
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-05-22 00:52:13 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,23 +1,35 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int value = (in.nextInt() * in.nextInt() * in.nextInt());
-		String str = Integer.toString(value);
-		in.close();
-		
-		for (int i = 0; i < 10; i++) {
-			int count = 0;
-			for (int j = 0; j < str.length(); j++) {
-				if ((str.charAt(j) - '0') == i) {
-					count++;
-				}
-			}
-			System.out.println(count);
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        int total = a * b * c;
+
+
+        String str = String.valueOf(total);
+        int[] arr = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = str.charAt(i) - '0';  // '0'을 빼서 정수화
+        }
+
+        for(int i=0;i<10;i++){  // 1부터 10까지
+            int count = 0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[j] == i){ // 만약 arr[i]가 i와 같다면
+                    count++; 
+                }
+            }
+            System.out.println(count);
+        }
+    }
 }
