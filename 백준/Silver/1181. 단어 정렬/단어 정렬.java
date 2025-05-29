23092d79d532fbe78f27e3edf1367ff90838486a
publicguard_ <100781721+SuOhYoon@users.noreply.github.com>
2025-05-29 09:16:07 +0900

    [Silver V] Title: 단어 정렬, Time: 428 ms, Memory: 22924 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
 
         int N = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
@@ -12,6 +13,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             set.add(br.readLine());
         }
+        br.close();
 
         // Set을 ArrayList로 변환
         List<String> list = new ArrayList<>(set);
